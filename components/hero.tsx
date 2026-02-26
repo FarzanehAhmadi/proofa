ea1e3ccbc99073b18c9ff84047a1771fec04907a
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2026-02-26 14:42:17 +0100

    feat: add Hero component and animated grid pattern; update Home page layout

---
+++ components/hero.tsx
@@ -0,0 +1,64 @@
+import { ArrowUpRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className="relative flex min-h-screen items-center justify-center overflow-hidden px-6">
+      <AnimatedGridPattern
+        className={cn(
+          "mask-[radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 h-full skew-y-12",
+        )}
+        duration={3}
+        maxOpacity={0.1}
+        numSquares={30}
+      />
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="mx-auto grid w-full max-w-(--breakpoint-xl) gap-12 px-6 py-12 lg:grid-cols-2">
+          <div>
+            <h1 className="mt-6 max-w-[17ch] font-semibold text-4xl leading-[1.2]! tracking-[-0.035em] md:text-5xl lg:text-[2.75rem] xl:text-[3.25rem]">
+              ZKP Blockchain Consultancy & Solutions
+            </h1>
+            <p className="mt-6 max-w-[60ch] text-foreground/80 sm:text-lg">
+              Expert post-quantum consultancy for blockchain and cryptography.
+              We help organisations navigate blockchain technology and prepare
+              for the quantum computing era with quantum-resistant solutions.
+            </p>
+            <div className="mt-12 flex items-center gap-4">
+              <Button className="rounded-full text-base" size="lg">
+                Book a Consultation <ArrowUpRight className="h-5! w-5!" />
+              </Button>
+              <Button
+                className="rounded-full text-base shadow-none"
+                size="lg"
+                variant="outline"
+              >
+                Explore Services
+              </Button>
+            </div>
+          </div>
+          {/* <div className="aspect-video w-full rounded-xl bg-accent" /> */}
+          <Image
+            src="/Logo.svg"
+            width={124}
+            height={32}
+            alt="Logo"
+            className="aspect-video w-full"
+          />
+          {/* <AnimatedGridPattern
+            className={cn(
+              "mask-[radial-gradient(500px_circle_at_center,white,transparent)]",
+              "inset-x-0 h-full skew-y-12",
+            )}
+            duration={3}
+            maxOpacity={0.1}
+            numSquares={30}
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
