25b8ad2ff849d566ff366c22af0a87d35b9de019
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2026-03-17 13:09:37 +0100

    feat: add Web3 integration with RainbowKit and Wagmi
    - Updated package.json to include dependencies for RainbowKit and Wagmi.
    - Created DappLayout component to wrap children with Web3Provider.
    - Added ConnectWallet component to facilitate wallet connection.
    - Implemented Web3Provider to manage Wagmi and RainbowKit providers.
    - Created a new page for the Dapp with a title and wallet connection button.

---
+++ app/dapps/page.tsx
@@ -0,0 +1,10 @@
+import ConnectWallet from "@/components/ConnectWallet";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>My Web3 Experiment</h1>
+      <ConnectWallet />
+    </main>
+  );
+}
---
+++ components/provider/Web3Provider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { mainnet } from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "Proofa",
+  projectId: "demo", // later you can add walletconnect id
+  chains: [mainnet],
+});
+
+const queryClient = new QueryClient();
+
+export default function Web3Provider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
